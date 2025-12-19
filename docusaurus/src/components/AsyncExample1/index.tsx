27f53163a6facfc72eeaef6098675e97e61b4760
Valentin Valls <valentin.valls@esrf.fr>
2025-12-19 21:22:16 +0100

    doc: Rework MockMotor as ts

--- docusaurus/src/components/AsyncExample1/index.tsx
+++ docusaurus/src/components/AsyncExample1/index.tsx
@@ -1,98 +1,101 @@
-import React from 'react'
-import { MockMotor } from '../../assets/MockMotor'
+import React from 'react';
+import { MockMotor } from '../../helpers/MockMotor';
 import { Knob, Value, Pointer, Range } from '@vallsv/rc-knob';
 
-function MotorKnob({
-  size,
-  value,
-  target=null,
-  onChange = () => { },
-}) {
-  const [editedValue, setEditedValue] = React.useState(null)
+function MotorKnob({ size, value, target = null, onChange = () => {} }) {
+    const [editedValue, setEditedValue] = React.useState(null);
 
-  function onKnobInteractiveChange(value) {
-    setEditedValue(value)
-  }
+    function onKnobInteractiveChange(value) {
+        setEditedValue(value);
+    }
 
-  function onKnobMouseUp(e) {
-    setEditedValue(null)
-  }
+    function onKnobMouseUp(e) {
+        setEditedValue(null);
+    }
 
-  const outerKnobRadius = size * 0.5;
-  const pointerSize = 10;
-  const pointerPosRadius = outerKnobRadius - pointerSize * 2;
+    const outerKnobRadius = size * 0.5;
+    const pointerSize = 10;
+    const pointerPosRadius = outerKnobRadius - pointerSize * 2;
 
-  const localTarget = (editedValue !== null) ? editedValue : target
+    const localTarget = editedValue !== null ? editedValue : target;
 
-  return (
-    <Knob
-      size={size}
-      value={value}
-      angleOffset={0}
-      angleRange={360}
-      min={0}
-      max={360}
-      onChange={onChange}
-      onInteractiveChange={onKnobInteractiveChange}
-      onEnd={onKnobMouseUp}
-      tracking={false}
-    >
-      <circle
-        r={pointerPosRadius + pointerSize}
-        cx={size / 2}
-        cy={size / 2}
-        fill="transparent"
-        stroke="#ced4da"
-        strokeWidth={pointerSize}
-      />
-      { localTarget !== null ? (<Range
-        percentageFrom={value / 360}
-        percentageTo={localTarget / 360}
-        radius={pointerPosRadius + pointerSize * 1.5}
-        color="#ff9800"
-        arcWidth={pointerSize}
-      />) : ""}
-      { localTarget !== null ? (<Pointer
-        percentage={localTarget / 360}
-        width={pointerSize}
-        height={pointerSize}
-        radius={pointerPosRadius}
-        type="circle"
-        color="#ff9800"
-      />) : ""}
-      <Pointer
-        percentage={value / 360}
-        width={pointerSize}
-        height={pointerSize}
-        radius={pointerPosRadius}
-        type="circle"
-        color="#000"
-      />
-      <Value
-        value={editedValue !== null ? editedValue : value}
-        decimalPlace={2}
-        marginBottom={size / 2}
-        className="knob-text"
-      />
-    </Knob>
-  )
+    return (
+        <Knob
+            size={size}
+            value={value}
+            angleOffset={0}
+            angleRange={360}
+            min={0}
+            max={360}
+            onChange={onChange}
+            onInteractiveChange={onKnobInteractiveChange}
+            onEnd={onKnobMouseUp}
+            tracking={false}
+        >
+            <circle
+                r={pointerPosRadius + pointerSize}
+                cx={size / 2}
+                cy={size / 2}
+                fill="transparent"
+                stroke="#ced4da"
+                strokeWidth={pointerSize}
+            />
+            {localTarget !== null ? (
+                <Range
+                    percentageFrom={value / 360}
+                    percentageTo={localTarget / 360}
+                    radius={pointerPosRadius + pointerSize * 1.5}
+                    color="#ff9800"
+                    arcWidth={pointerSize}
+                />
+            ) : (
+                ''
+            )}
+            {localTarget !== null ? (
+                <Pointer
+                    percentage={localTarget / 360}
+                    width={pointerSize}
+                    height={pointerSize}
+                    radius={pointerPosRadius}
+                    type="circle"
+                    color="#ff9800"
+                />
+            ) : (
+                ''
+            )}
+            <Pointer
+                percentage={value / 360}
+                width={pointerSize}
+                height={pointerSize}
+                radius={pointerPosRadius}
+                type="circle"
+                color="#000"
+            />
+            <Value
+                value={editedValue !== null ? editedValue : value}
+                decimalPlace={2}
+                marginBottom={size / 2}
+                className="knob-text"
+            />
+        </Knob>
+    );
 }
 
 export default function Example() {
-    const initialPosition = 45
-    const [position, setPosition] = React.useState(initialPosition)
-    const [target, setTarget] = React.useState(null)
+    const initialPosition = 45;
+    const [position, setPosition] = React.useState(initialPosition);
+    const [target, setTarget] = React.useState(null);
 
     function onKnobChange(position) {
-        setTarget(position)
+        setTarget(position);
     }
 
     function onMotorChange(position) {
-        setPosition(position)
+        setPosition(position);
     }
 
     return (
-	    <div>
+        <div>
             <MotorKnob
                 size={120}
                 target={target}
@@ -101,13 +104,13 @@ export default function Example() {
                 max={9999}
                 value={position}
                 onChange={onKnobChange}
-                />
+            />
             <MockMotor
                 initialPosition={initialPosition}
                 onChange={onMotorChange}
                 velocity={30}
                 target={target}
-                />
+            />
         </div>
-    )
+    );
 }
---
+++ docusaurus/src/helpers/MockMotor.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+export function MockMotor(props: {
+    initialPosition: number;
+    target?: number;
+    velocity?: number;
+    onChange?: (position: number) => void;
+}) {
+    const {
+        initialPosition,
+        target = null,
+        velocity = 1,
+        onChange = () => {},
+    } = props;
+    const now = Date.now();
+    const [internalPosition, setInternalPosition] =
+        React.useState(initialPosition);
+    const [internalTime, setInternalTime] = React.useState(now);
+
+    const validTarget = (target, position) => {
+        return target !== null && target !== position;
+    };
+
+    const updatePosition = () => {
+        const now = Date.now();
+        const deltaTime = (now - internalTime) / 1000;
+        let position = internalPosition;
+        setInternalTime(now);
+        if (validTarget(target, position)) {
+            const deltaPosition = deltaTime * velocity;
+            if (deltaPosition > Math.abs(position - target)) {
+                position = target;
+            } else {
+                const direction = target > position ? 1 : -1;
+                position = position + direction * deltaPosition;
+            }
+            setInternalPosition(position);
+            onChange(position);
+        }
+    };
+
+    React.useEffect(() => {
+        const timer = setInterval(() => {
+            updatePosition();
+        }, 100);
+        return () => clearInterval(timer);
+    }, [target, internalTime, internalPosition]);
+
+    return (
+        <div>
+            Position: {internalPosition.toFixed(2)} <br />
+            Target:{' '}
+            {validTarget(target, internalPosition) ? target.toFixed(2) : '----'}
+        </div>
+    );
+}
