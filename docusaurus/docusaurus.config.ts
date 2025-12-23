c09e7aadf25bc57dfa802c6b6dfb068f58a9a0c6
Valentin Valls <valentin.valls@esrf.fr>
2025-12-23 20:31:29 +0100

    doc: Formating

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -1,73 +1,73 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'rc-knob-docs',
-  tagline: 'rc-knob documentation',
-  favicon: 'img/favicon.png',
+    title: 'rc-knob-docs',
+    tagline: 'rc-knob documentation',
+    favicon: 'img/favicon.png',
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-  future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
-  },
+    // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+    future: {
+        v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    },
 
-  // Set the production url of your site here
-  url: 'https://vallsv.github.io/',
-  baseUrl: '/rc-knob/',
+    // Set the production url of your site here
+    url: 'https://vallsv.github.io/',
+    baseUrl: '/rc-knob/',
 
-  organizationName: '@vallsv/rc-knob',
-  projectName: '@vallsv/rc-knob',
+    organizationName: '@vallsv/rc-knob',
+    projectName: '@vallsv/rc-knob',
 
-  onBrokenLinks: 'throw',
+    onBrokenLinks: 'throw',
 
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
 
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          routeBasePath: '/',
-          sidebarPath: './sidebars.ts',
-          breadcrumbs: false,
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      } satisfies Preset.Options,
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    routeBasePath: '/',
+                    sidebarPath: './sidebars.ts',
+                    breadcrumbs: false,
+                },
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
     ],
-  ],
 
-  themeConfig: {
-    colorMode: {
-      respectPrefersColorScheme: true,
-    },
-    navbar: {
-      title: '@vallsv/rc-knob',
-      logo: {
-        alt: 'knob logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          href: 'https://github.com/vallsv/rc-knob',
-          label: 'GitHub',
-          position: 'right',
+    themeConfig: {
+        colorMode: {
+            respectPrefersColorScheme: true,
         },
-      ],
-    },
-    footer: {
-      style: 'dark',
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+        navbar: {
+            title: '@vallsv/rc-knob',
+            logo: {
+                alt: 'knob logo',
+                src: 'img/logo.svg',
+            },
+            items: [
+                {
+                    href: 'https://github.com/vallsv/rc-knob',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
+        },
+        footer: {
+            style: 'dark',
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
 };
 
 export default config;
